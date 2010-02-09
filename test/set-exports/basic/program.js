62b619e0e3ed6193b39e7764fea84ecf5ea3c98b
Patrick Mueller <pmuellr@yahoo.com>
2010-02-09 03:47:41 -0500

    make tests runnable on an iPhone

--- test/set-exports/basic/program.js
+++ test/set-exports/basic/program.js
@@ -1,5 +1,4 @@
 var test = require('test');
 var foo = require('foo');
-test.print("foo: " + JSON.stringify(foo))
 test.assert(foo == 234, 'basic use of setExports works');
 test.print('DONE', 'info');
